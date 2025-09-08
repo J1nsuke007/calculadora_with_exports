cdd4c8a25b014b5e09d56a1aa1769681118961e5
J1nsuke007 <angelousdxd@gmail.com>
2025-09-08 16:19:54 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 const math = require("./math.js");
 
 console.log(`La opcion que eligio fue la numero ${math.Tabla(3)} y el resultado fue ${math.multiplicacion(5,4)}`)
+//puedes cambiar la opcion en math.Tabla y cambiar los numeros en math.las cuatro operaciones
