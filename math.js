260f35d191970a2100320a9212dd46fb6e9bbe83
J1nsuke007 <angelousdxd@gmail.com>
2025-09-08 16:19:08 -0500

    Update math.js

--- math.js
+++ math.js
@@ -4,24 +4,25 @@ function Tabla(opcion) {
             function suma(a,b) {
                 return a + b;
             }
-            exports.suma = suma;
+            exports.suma = suma;//exportamos la funcion suma
             break;
         case 2:
             function resta(a,b) {
                 return a - b;
             }
-            exports.resta = resta;
+            exports.resta = resta;//exportamos la funcion resta
             break;
         case 3:
             function multiplicacion(a,b) {
                 return a * b;
             }
-            exports.multiplicacion = multiplicacion;
+            exports.multiplicacion = multiplicacion;//exportamos la funcion multiplicacion
             break;
         case 4:
             function division(a,b) {
                 if (b === 0) {
-                    console.log("no se puede dividir por cero");
+                    console.log("no se puede dividir por cero");//al momento de dividir entre 0 nos sale un error en la consola
+                    //para evitar que rompa el codigo, mejos usamos una condicional
                 } else {
                     return a / b;
                     
